ce2d5b8defc19f3f4ebba49f6fd33110c35e35a9
allen_yu <wiamyu@gmail.com>
2017-06-13 16:07:36 +0800

    init

---
+++ MetaData/Models/MetaData/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetaData.Models
+{
+    [MetadataType(typeof(OrdersMetadata))]
+    public partial class Orders
+    {
+        public class OrdersMetadata
+        {
+            [DisplayName("測試測試")]
+            [Range(1, 4)]
+            public Nullable<decimal> Freight { get; set; }
+        }
+    }
+}
